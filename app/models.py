d00cea393f1596f2b1a236f9e2dc49a2761fb702
A.R.T. van der Sterren <a.r.t.van.der.sterren@umail.leidenuniv.nl>
2024-04-08 16:42:49 +0200

    Update file models.py

--- app/models.py
+++ app/models.py
@@ -7,6 +7,11 @@ from app import db
 from app import login
 from flask_login import UserMixin
 
+favourite_chats = db.Table('favourite_chats',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('chat_id', db.Integer, db.ForeignKey('chat.id'))
+)
+
 
 class User(UserMixin, db.Model):
     def set_password(self, password):
@@ -25,8 +30,40 @@ class User(UserMixin, db.Model):
     posts: so.WriteOnlyMapped['Post'] = so.relationship(
         back_populates='author')
 
+    sent_requests = so.relationship(
+        'FriendRequest',
+        foreign_keys='FriendRequest.from_user_id',
+        backref='from_user_backref',
+        lazy='dynamic'
+    )
+
+    received_requests = so.relationship(
+        'FriendRequest',
+        foreign_keys='FriendRequest.to_user_id',
+        backref='to_user_backref',
+        lazy='dynamic'
+    )
+
+    favourite_chats = db.relationship(
+        'Chat',
+        secondary=favourite_chats,
+        backref=db.backref('favourited_by', lazy='dynamic')
+    )
+
+    @property
+    def pending_requests(self):
+        return FriendRequest.query.filter_by(to_user_id=self.id, status=0)
+
+    @property
+    def friends(self):
+        return User.query.join(
+            FriendRequest, (FriendRequest.from_user_id == User.id) | (FriendRequest.to_user_id == User.id)
+        ).filter(FriendRequest.status == 1)
+
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
 
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
@@ -40,6 +77,54 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+class Chat(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    users: so.WriteOnlyMapped['User'] = so.relationship(
+        'User',
+        secondary='chat_users',
+        backref=so.backref('chats', lazy='dynamic')
+    )
+    messages: so.WriteOnlyMapped['Message'] = so.relationship('Message', backref='chat', lazy='dynamic')
+
+class Message(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    text: so.Mapped[str] = so.mapped_column(sa.String(500))
+    timestamp: so.Mapped[datetime] = so.mapped_column(
+        index=True, default=lambda: datetime.now(timezone.utc))
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id))
+    chat_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Chat.id))
+
+    user: so.Mapped[User] = so.relationship('User', backref='messages')
+
+# Association table for many-to-many relationship between User and Chat
+chat_users = db.Table('chat_users',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('chat_id', db.Integer, db.ForeignKey('chat.id'))
+)
+
+
+class Friendship(db.Model):
+    __tablename__ = 'friendship'
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    friend_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    user = db.relationship('User', foreign_keys=[user_id], backref=db.backref('friends', lazy='dynamic'))
+
+    def __repr__(self):
+        return '<Friendship {} - {}>'.format(self.user_id, self.friend_id)
+
+
+
+class FriendRequest(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    from_user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id))
+    to_user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id))
+    status: so.Mapped[int] = so.mapped_column(sa.Integer, default=0)  # 0 for pending, 1 for accepted
+
+    from_user: so.Mapped[User] = so.relationship(User, foreign_keys=[from_user_id], backref='from_user_backref')
+    to_user: so.Mapped[User] = so.relationship(User, foreign_keys=[to_user_id], backref='to_user_backref')
 
 @login.user_loader
 def load_user(id):
